07700ba85d0c409d8c6298262527d2c11cb2fbf8
Manas Nikte <80404462+ManasNikte@users.noreply.github.com>
2022-04-02 16:55:19 +0530

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://pure-bayou-69390.herokuapp.com/register', {
+    fetch('https://shrouded-springs-54542.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
