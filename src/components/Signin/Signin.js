009701914a9c1707810997e47bc2fcdb47586f01
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-04-09 17:48:41 +0530

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch(`${process.env.API_BACKEND}/signin`, {
+    fetch(`${process.env.REACT_APP_BACKEND}/signin`, {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
